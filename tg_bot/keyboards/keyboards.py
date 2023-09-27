10821d2f3fc9e35ebde051a49f3fb256ab0468ee
agosset15 <ag@agosset.ru>
2023-09-27 16:01:08 +0300

    Исправлена опечатка

--- tg_bot/keyboards/keyboards.py
+++ tg_bot/keyboards/keyboards.py
@@ -261,9 +261,9 @@ def kab_free_kb():
 
 def kab_free_lessons(day: int = None):
     kb = InlineKeyboardBuilder()
-    t = day if day else "_today"
+    t = day if day else "today"
     for i in range(1, 9):
-        kb.button(text=f"{i}", callback_data=f"{i}{t}_kab_free")
+        kb.button(text=f"{i}", callback_data=f"{i}_{t}_kab_free")
     kb.adjust(4)
     return kb.as_markup()
 
