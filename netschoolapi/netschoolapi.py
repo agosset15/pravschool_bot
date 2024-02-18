bfa7c2f801042c08136896059abd4dcd1f2b3c44
agosset15 <ag@agosset.ru>
2024-02-18 16:08:19 +0300

    Исправление ошибок

--- netschoolapi/netschoolapi.py
+++ netschoolapi/netschoolapi.py
@@ -325,10 +325,7 @@ class NetSchoolAPI:
                    "pas": {"hash": ast.literal_eval(self._login_data[1])[0]}}
         file = request('GET', 'http://127.0.0.1:3000/report',
                        params={'logi': self._login_data[0],
-                               'uri': f"{report_url}/queue"}, data=payload, timeout=1000, headers=
-                       {'content-type': 'application/json', 'user-agent': 'PostmanRuntime/7.36.1', 'accept': '*/*',
-                        'cache-control': 'no-cache', 'host': '127.0.0.1:3000', 'accept-encoding': 'gzip, deflate, br',
-                        'connection': 'keep-alive'})
+                               'uri': f"{report_url}/queue"}, data=payload, timeout=1000)
         return file.text
 
     async def school(self, requests_timeout: int = None) -> schemas.School:
