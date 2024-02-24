d667ffebfab13aa36616641808b05eab8c6274a7
agosset15 <ag@agosset.ru>
2024-02-24 18:01:45 +0300

    Исправление ошибок

--- tg_bot/web_routes/get.py
+++ tg_bot/web_routes/get.py
@@ -1,6 +1,7 @@
 import ast
 import json
 import datetime
+import random
 from pathlib import Path
 
 from aiogram.utils.web_app import safe_parse_webapp_init_data, check_webapp_signature
@@ -247,6 +248,26 @@ async def getdb_rasp_today(request: Request):
     if tomorrow:
         date = datetime.date.today().weekday() + 2
     tomorrow = {True: "Завтра", False: "Сегодня"}[tomorrow]
+    if date > 5:
+        return json_response(body=str({"ok": True, "rasp": "Выходной!", "tomorrow": tomorrow}).encode())
+    if usr.isTeacher is True:
+        rasp = get_teacher_schedule(usr.clas, date)
+    else:
+        rasp = get_schedule(usr.clas, date)
+    res = ''.join(ast.literal_eval(rasp))
+    return json_response(body=str({"ok": "True", "rasp": res, "tomorrow": tomorrow}).encode())
+
+
+async def getdb_rasp_random(request: Request):
+    data = request.query
+    try:
+        usr = get_student_by_telegram_id(int(data['tgid']))
+        if usr is None:
+            return json_response({"ok": False, "err": "Unauthorized"}, status=401)
+    except ValueError:
+        return json_response({"ok": False, "err": "Unauthorized"}, status=401)
+    date = random.randint(1, 7)
+    tomorrow = random.choice(["Завтра", "Сегодня"])
     if date > 5:
         return json_response(body=str({"ok": True, "rasp": "Выходной!", "tomorrow": tomorrow}).encode())
     if usr.isTeacher is True:
